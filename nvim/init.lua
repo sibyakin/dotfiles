4d4d738fd1fa283439cf744705c57a83801bbd15
Alex Sibyakin <sibyakin@yahoo.com>
2024-09-01 23:49:21 +0900

    small fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
---main
 vim.o.sessionoptions='blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
@@ -18,30 +17,27 @@ vim.cmd('command! W :w')
 vim.cmd('command! Q :q')
 vim.cmd('colo darcula-dark')
 
---modules
 require('paq')({
-    {'savq/paq-nvim'},
+    {'neovim/nvim-lspconfig'},
     {'nvim-lua/plenary.nvim'},
     {'nvim-tree/nvim-web-devicons'},
+    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     {'nvim-lualine/lualine.nvim'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lsp-signature-help'},
     {'dcampos/nvim-snippy'},
     {'dcampos/cmp-snippy'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lsp-signature-help'},
     {'hrsh7th/nvim-cmp'},
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     {'windwp/nvim-autopairs'},
     {'rmagatti/auto-session'},
     {'lewis6991/gitsigns.nvim'},
     {'nvim-telescope/telescope.nvim'},
     {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
-    {'nvim-telescope/telescope-ui-select.nvim'},
-    {'j-hui/fidget.nvim'},
     {'xiantang/darcula-dark.nvim'},
+    {'j-hui/fidget.nvim'},
+    {'savq/paq-nvim'},
 })
 
---lsp+cmp
 local lsp_on_attach = function()
     vim.diagnostic.config({signs = false, virtual_text = false})
 
@@ -66,12 +62,10 @@ lsp.gopls.setup({
                 for cid, res in pairs(result or {}) do
                     for _, r in pairs(res.result or {}) do
                         if r.edit then
-                            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
-                            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                            vim.lsp.util.apply_workspace_edit(r.edit, 'utf-8')
                         end
                     end
                 end
-                vim.lsp.buf.format({async = false})
             end
         })
     end
@@ -82,30 +76,20 @@ snippy.setup({})
 
 local cmp = require('cmp')
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            snippy.expand_snippet(args.body)
-        end,
-    },
     mapping = cmp.mapping{
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item()
+                if #cmp.get_entries() == 1 then
+                    cmp.confirm({select = true})
+                else
+                    cmp.select_next_item()
+                end
             elseif snippy.can_expand_or_advance() then
                 snippy.expand_or_advance()
             else
                 fallback()
             end
         end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif snippy.can_jump(-1) then
-                snippy.previous()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
         ['<C-d>'] = cmp.mapping.confirm({select = true}),
     },
     sources = {
@@ -115,35 +99,26 @@ cmp.setup({
     },
 })
 
---ui
-require('auto-session').setup({auto_restore_last_session = true, lazy_support = false})
-require('gitsigns').setup({current_line_blame = true})
 require('nvim-treesitter.configs').setup({
     ensure_installed = {'go', 'gomod', 'gotmpl'},
     highlight = {enable = true},
 })
-require('nvim-web-devicons').setup()
-require('nvim-autopairs').setup()
+require('auto-session').setup({auto_restore_last_session = true})
+require('nvim-web-devicons').setup({default = true, strict = true})
+require('lualine').setup({sections = {lualine_c = {{'filename', path = 1}}}})
 require('fidget').setup({
     notification = {
         override_vim_notify = true,
         view = {stack_upwards = false}, 
-        window = {winblend = 0, max_width = 100},
+        window = {winblend = 0},
     },
 })
-require('lualine').setup({sections = {lualine_c = {{'filename', path = 1}}}})
 local telescope = require('telescope')
-telescope.setup({
-    defaults = {
-        layout_config = {height = vim.o.lines-5, width = vim.o.columns-20},
-        preview = {treesitter = false, hide_on_startup = true},
-    },
-    pickers = {diagnostics = {line_width = 0.99}},
-})
+telescope.setup({defaults = {preview = {hide_on_startup = true}}})
 telescope.load_extension('fzf')
-telescope.load_extension('ui-select')
+require('gitsigns').setup({current_line_blame = true})
+require('nvim-autopairs').setup()
 
---key bindings
 vim.keymap.set('n', 'fb', '<cmd>Telescope buffers<CR>')
 vim.keymap.set('n', 'fc', '<cmd>Telescope oldfiles<CR>')
 vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<CR>')
